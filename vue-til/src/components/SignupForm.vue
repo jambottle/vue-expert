9b700fede63d99b20d8933d0764fcf541bf48f69
jambottle <jaewon.walks@gmail.com>
2022-04-27 00:15:02 +0900

    feat: Use v-model directive for two-way binding with 3 form data

--- vue-til/src/components/SignupForm.vue
+++ vue-til/src/components/SignupForm.vue
@@ -2,16 +2,28 @@
   <form>
     <div>
       <label for="username">ID: </label>
-      <input id="username" type="text" />
+      <input id="username" type="text" v-model="username" />
     </div>
     <div>
       <label for="password">PW: </label>
-      <input id="password" type="text" />
+      <input id="password" type="text" v-model="password" />
     </div>
     <div>
       <label for="nickname">Nickname: </label>
-      <input id="nickname" type="text" />
+      <input id="nickname" type="text" v-model="nickname" />
     </div>
     <button type="submit">Sign Up</button>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      nickname: '',
+    };
+  },
+};
+</script>
