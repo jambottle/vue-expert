999cbc3f8b274daa24a4c718e39f63df8e253efa
jambottle <jaewon.walks@gmail.com>
2022-04-29 23:15:02 +0900

    feat: Use v-model directive for two-way binding with 2 form data

--- vue-til/src/components/SigninForm.vue
+++ vue-til/src/components/SigninForm.vue
@@ -2,12 +2,23 @@
   <form>
     <div>
       <label for="username">ID: </label>
-      <input id="username" type="text" />
+      <input id="username" type="text" v-model="username" />
     </div>
     <div>
       <label for="password">PW: </label>
-      <input id="password" type="text" />
+      <input id="password" type="text" v-model="password" />
     </div>
     <button type="submit">Sign In</button>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+};
+</script>
