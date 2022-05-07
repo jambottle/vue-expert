24633ec04b4b272a205e616571aa8d7049b6d053
jambottle <jaewon.walks@gmail.com>
2022-05-08 00:30:24 +0900

    design: Style SigninPage.vue and SignupPage.vue with BEM convention

--- vue-til/src/views/SigninPage.vue
+++ vue-til/src/views/SigninPage.vue
@@ -1,6 +1,8 @@
 <template>
-  <h1>Sign In Now</h1>
-  <SigninForm />
+  <main class="page">
+    <h1 class="page__title">Sign In Now</h1>
+    <SigninForm />
+  </main>
 </template>
 
 <script>
@@ -14,11 +16,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-h1 {
-  padding: 30px 5px 15px;
-  color: #456155;
-  font-size: 45px;
-  font-weight: 600;
-  text-align: center;
+.page {
+  max-width: 1020px;
+  width: 100%;
+  margin: 60px auto;
+
+  &__title {
+    padding: 30px 5px 15px;
+    color: #456155;
+    font-size: 45px;
+    font-weight: 600;
+    text-align: center;
+  }
 }
 </style>
--- vue-til/src/views/SignupPage.vue
+++ vue-til/src/views/SignupPage.vue
@@ -1,6 +1,8 @@
 <template>
-  <h1>Create Your Account</h1>
-  <SignupForm />
+  <main class="page">
+    <h1 class="page__title">Create Your Account</h1>
+    <SignupForm />
+  </main>
 </template>
 
 <script>
@@ -14,11 +16,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-h1 {
-  padding: 30px 5px 15px;
-  color: #456155;
-  font-size: 45px;
-  font-weight: 600;
-  text-align: center;
+.page {
+  max-width: 1020px;
+  width: 100%;
+  margin: 60px auto;
+
+  &__title {
+    padding: 30px 5px 15px;
+    color: #456155;
+    font-size: 45px;
+    font-weight: 600;
+    text-align: center;
+  }
 }
 </style>
