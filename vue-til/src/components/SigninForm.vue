e071eaf938e89f916b733922d380fa632df63ec9
jambottle <jaewon.walks@gmail.com>
2022-05-08 00:25:17 +0900

    design: Style SigninForm.vue and SignupForm.vue with BEM convention

--- vue-til/src/components/SigninForm.vue
+++ vue-til/src/components/SigninForm.vue
@@ -1,21 +1,40 @@
 <template>
-  <!-- form section -->
-  <form @submit.prevent="submitForm">
-    <div>
-      <label for="username">ID: </label>
-      <input id="username" type="text" v-model="username" />
-    </div>
-    <div>
-      <label for="password">PW: </label>
-      <input id="password" type="text" v-model="password" />
-    </div>
-    <button type="submit" :disabled="!isUsernameValid || !password">
-      Sign In
-    </button>
-  </form>
+  <section class="form-wrapper">
+    <!-- form section -->
+    <form class="form" @submit.prevent="submitForm">
+      <div>
+        <label class="form__label" for="username">ID: </label>
+        <input
+          class="form__input"
+          id="username"
+          type="text"
+          v-model="username"
+        />
+        <p class="validation-text" v-if="!isUsernameValid && username">
+          Please enter a valid email address.
+        </p>
+      </div>
+      <div>
+        <label class="form__label" for="password">PW: </label>
+        <input
+          class="form__input"
+          id="password"
+          type="text"
+          v-model="password"
+        />
+      </div>
+      <button
+        class="form__button"
+        type="submit"
+        :disabled="!isUsernameValid || !password"
+      >
+        Sign In
+      </button>
+    </form>
 
-  <!-- log section -->
-  <p>{{ logMessage }}</p>
+    <!-- log section -->
+    <p class="log-message">{{ logMessage }}</p>
+  </section>
 </template>
 
 <script>
@@ -62,3 +81,79 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.form-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  max-width: 500px;
+  width: 100%;
+  height: 100%;
+  margin: 40px auto;
+  border-radius: 3px;
+  padding: 15px 20px;
+
+  background-color: #ffffff;
+  box-shadow: 0 20px 20px rgba(0, 0, 0, 0.08);
+  -webkit-box-shadow: 0 20px 20px rgba(0, 0, 0, 0.08);
+
+  .form {
+    max-width: 460px;
+    width: 100%;
+    height: 100%;
+
+    &__label {
+      display: block;
+      width: 100%;
+      margin-bottom: 0.5rem;
+      color: #364f6b;
+      font-size: 90%;
+    }
+
+    &__input {
+      width: 100%;
+      margin-bottom: 1rem;
+      border: 1px solid #dae1e7;
+      padding: 0.5rem 0.75rem;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+      font-family: inherit;
+      font-size: 100%;
+    }
+
+    .validation-text {
+      display: flex;
+      flex-direction: row-reverse;
+      justify-content: space-between;
+      margin-top: -0.5rem;
+      margin-bottom: 0.5rem;
+      color: #ff4057;
+      font-size: 1rem;
+    }
+
+    &__button {
+      border: 0 solid #dae1e7;
+      border-radius: 0.25rem;
+      padding: 0.5rem 1.5rem;
+      background-color: #fe9616;
+      color: #ffffff;
+      font-weight: 700;
+      cursor: pointer;
+
+      &:disabled {
+        opacity: 0.5;
+        cursor: not-allowed;
+      }
+    }
+  }
+
+  .log-message {
+    margin-top: 1.5rem;
+    color: #ff4057;
+    font-size: 1rem;
+    text-align: center;
+  }
+}
+</style>
--- vue-til/src/components/SignupForm.vue
+++ vue-til/src/components/SignupForm.vue
@@ -1,28 +1,49 @@
 <template>
-  <!-- form section -->
-  <form @submit.prevent="submitForm">
-    <div>
-      <label for="username">ID: </label>
-      <input id="username" type="text" v-model="username" />
-    </div>
-    <div>
-      <label for="password">PW: </label>
-      <input id="password" type="text" v-model="password" />
-    </div>
-    <div>
-      <label for="nickname">Nickname: </label>
-      <input id="nickname" type="text" v-model="nickname" />
-    </div>
-    <button
-      type="submit"
-      :disabled="!isUsernameValid || !password || !nickname"
-    >
-      Sign Up
-    </button>
-  </form>
+  <section class="form-wrapper">
+    <!-- form section -->
+    <form class="form" @submit.prevent="submitForm">
+      <div>
+        <label class="form__label" for="username">ID: </label>
+        <input
+          class="form__input"
+          id="username"
+          type="text"
+          v-model="username"
+        />
+        <p class="validation-text" v-if="!isUsernameValid && username">
+          Please enter a valid email address.
+        </p>
+      </div>
+      <div>
+        <label class="form__label" for="password">PW: </label>
+        <input
+          class="form__input"
+          id="password"
+          type="text"
+          v-model="password"
+        />
+      </div>
+      <div>
+        <label class="form__label" for="nickname">Nickname: </label>
+        <input
+          class="form__input"
+          id="nickname"
+          type="text"
+          v-model="nickname"
+        />
+      </div>
+      <button
+        class="form__button"
+        type="submit"
+        :disabled="!isUsernameValid || !password || !nickname"
+      >
+        Sign Up
+      </button>
+    </form>
 
-  <!-- log section -->
-  <p>{{ logMessage }}</p>
+    <!-- log section -->
+    <p class="log-message">{{ logMessage }}</p>
+  </section>
 </template>
 
 <script>
@@ -72,3 +93,83 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.form-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  max-width: 500px;
+  width: 100%;
+  height: 100%;
+  margin: 40px auto;
+  border-radius: 3px;
+  padding: 15px 20px;
+
+  background-color: #ffffff;
+  box-shadow: 0 20px 20px rgba(0, 0, 0, 0.08);
+  -webkit-box-shadow: 0 20px 20px rgba(0, 0, 0, 0.08);
+
+  .form {
+    max-width: 460px;
+    width: 100%;
+    height: 100%;
+
+    div:nth-child(2) {
+      margin-bottom: 0.5rem;
+    }
+
+    &__label {
+      display: block;
+      width: 100%;
+      margin-bottom: 0.5rem;
+      color: #364f6b;
+      font-size: 90%;
+    }
+
+    &__input {
+      width: 100%;
+      margin-bottom: 1rem;
+      border: 1px solid #dae1e7;
+      padding: 0.5rem 0.75rem;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+      font-family: inherit;
+      font-size: 100%;
+    }
+
+    .validation-text {
+      display: flex;
+      flex-direction: row-reverse;
+      justify-content: space-between;
+      margin-top: -0.5rem;
+      margin-bottom: 0.5rem;
+      color: #ff4057;
+      font-size: 1rem;
+    }
+
+    &__button {
+      border: 0 solid #dae1e7;
+      border-radius: 0.25rem;
+      padding: 0.5rem 1.5rem;
+      background-color: #fe9616;
+      color: #ffffff;
+      font-weight: 700;
+      cursor: pointer;
+
+      &:disabled {
+        opacity: 0.5;
+        cursor: not-allowed;
+      }
+    }
+  }
+
+  .log-message {
+    margin-top: 1.5rem;
+    color: #ff4057;
+    font-size: 1rem;
+    text-align: center;
+  }
+}
+</style>
